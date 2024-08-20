5249d50dd858e2cba3b7c8044ec504b9436ba965
UraniumSlashBroomer <milkorehov@gmail.com>
2024-08-20 14:50:40 +0300

    slider

--- Scripts/Music/global_music.gd
+++ Scripts/Music/global_music.gd
@@ -2,7 +2,7 @@ extends AudioStreamPlayer
 
 const level_music = preload("res://Resources/Music/UNIVERSFIELD - Crime Drama.mp3")
 
-func _play_music(music: AudioStream, volume = 0.0):
+func _play_music(music: AudioStream, volume = -10.0):
 	if stream == music:
 		return
 		
--- Scripts/Music/music.gd
+++ Scripts/Music/music.gd
@@ -3,4 +3,3 @@ extends Node2D
 
 func _ready() -> void:
 	GlobalMusic.play_music_level()
-	GlobalMusic.change_volume(-10)
